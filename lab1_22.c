48d1bc0f3c8c0abbc4e8ef5da69539ac9479fdac
Andrea <andrea.storace.uni@gmail.com>
2017-10-14 15:05:38 +0200

    Update lab1_22.c

--- lab1_22.c
+++ lab1_22.c
@@ -21,14 +21,14 @@ int main(){
 		printf("nano-bash $ ");
 		if(fgets(option, 123, stdin)==NULL || strcmp("exit\n", option)==0) exit(EXIT_SUCCESS);
 		option[strlen(option)-1]='\0'; /* delete \n char */
-		memcpy(command, prefix, strlen(prefix));
+		memcpy(command, prefix, sizeof(prefix));
 		strcat(command, option);
 		pid = fork();
 		if(pid>=0){
 			if(pid==0){
 				p1ex = execl(command, command, NULL);
 				if(p1ex<0){
-					memcpy(command, prefix2, strlen(prefix2));
+					memcpy(command, prefix2, sizeof(prefix2));
 					strcat(command, option);
 					p2ex = execl(command, command, NULL);
 					if(p2ex <0)custom_perror("exec failed");
