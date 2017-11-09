1a7acbc76e52fee104b9f61aec7ccd7e9ad82004
Strafo <aspem2015@gmail.com>
2017-11-09 16:36:00 +0100

    Update banda_latenza_graph.bash
    che bello! sono proprio felicio

--- pingpong/scripts/banda_latenza_graph.bash
+++ pingpong/scripts/banda_latenza_graph.bash
@@ -4,64 +4,57 @@ set -e
 
 declare -a arr=("tcp" "udp")
 readonly DataDir='../data' 
-declare -a L
-declare -a B
-declare -a N1
-declare -a N2
-declare -a T1
-declare -a T2
-declare -a Denominatore
+
 
 
 for ProtocolName in "${arr[@]}"
 do
-    declare InputFile="${DataDir}/${ProtocolName}_throughput.dat"
-    declare OutputPngFile="${DataDir}/${ProtocolName}_banda_latenza.png"
-    declare OutputDatFile="${DataDir}/${ProtocolName}_delay.dat"
-    
-    if [ -e $OutputDatFile ]
-    then
-    	echo removing older version:
-    	rm $OutputPngFile $OutputDatFile -f
+    InputFile="${DataDir}/${ProtocolName}_throughput.dat"
+    OutputPngFile="${DataDir}/${ProtocolName}_banda_latenza.png"
+    OutputDatFile="${DataDir}/${ProtocolName}_delay.dat"
+    if [ -e ${OutputDatFile} ]
+     then 
+        rm -f ${Outputdatfile} 
+        rm -f ${OutputPngFile} 
     fi
-
     #ottengo parametri
     #f2 0 f3 cioè devo usare T come valore mediano o medio?
-    # it's fun to find out you actually don't know what to do with these variables;
-    # even funnier is the fact you don't even know which variable to use. I want to cry rn.
-    
-	# estraggo no byte del minimo:
-    N1=$(head -n 1 ${DataDir}/${ProtocolName}_throughput.dat | cut -d' ' -f1)
-    # estraggo delay del minimo:
-    T1=$(head -n 1 ${DataDir}/${ProtocolName}_throughput.dat | cut -d' ' -f2)
-    
-	# estraggo no byte del massimo:
-    N2=$(tail -n 1 ${DataDir}/${ProtocolName}_throughput.dat | cut -d' ' -f1)
-    # estraggo delay del massimo:
-    T2=$(tail -n 1 ${DataDir}/${ProtocolName}_throughput.dat | cut -d' ' -f2)
-    
+    N1=$(head -n 1 ${DataDir}/${ProtocolName}_throughput.dat | cut -d' ' -f1 )
+    T1=$(head -n 1 ${DataDir}/${ProtocolName}_throughput.dat | cut -d' ' -f3 )
+    N2=$(tail -n 1 ${DataDir}/${ProtocolName}_throughput.dat | cut -d' ' -f1 )
+    T2=$(tail -n 1 ${DataDir}/${ProtocolName}_throughput.dat | cut -d' ' -f3 )
+    echo "N1 T1 N2 T2:"
+    echo ${N1}
+    echo ${T1}
+    echo ${N2}
+    echo ${T2}
     #calcolo le costanti
     echo 'calcolo costanti'
-    DelayMin=$(bc <<<"scale=20;var1=${N1};var2=${T1};var1 / var2")
-    DelayMax=$(bc <<<"scale=20;var1=${N2};var2=${T2};var1 / var2")
-    Denominatore=$(bc <<< "scale=20;${DelayMax}-${DelayMin}")
-    
-    L=$(bc <<< "scale=20;var1=${DelayMax}*${N2};var2=${DelayMin}*${N1};var3=var1-var2;var3/${Denominatore}")
-    B=$(bc <<< "scale=20;var1=${N2}-${N1};var1/${Denominatore}")
+    DelayMin=$(bc <<<"scale=10;${N1}/${T1}")
+    DelayMax=$(bc <<<"scale=10;${N2}/${T2}")
+    Denominatore=$(bc <<< "scale=10;${DelayMax}-${DelayMin}")
+    echo "Delay min:${DelayMin} Delay max:${DelayMax}"
+    L=$(bc <<< "scale=10;( ( ${DelayMin} * ${N2} ) - ( ${DelayMax} * ${N1} ) ) / ( ${N2} - ${N1} )")
+    B=$(bc <<< "scale=10;(${N2}-${N1})/${Denominatore}")
+    echo $L
+    echo $B
 
-    echo bandwiwidth: $B
-    echo latency: $L
     #stampa i valori Numero_byte e Latenza sul file .dat
-    echo 'stampo i valori Numero_byte e Latenza sul file .dat'
-    N_LINEE_FILE=$(wc -l "${DataDir}/${ProtocolName}_throughput.dat" | cut -d ' ' -f1)
+    echo 'stampo i valori Numero_byte e Banda-Latenza sul file .dat'
+    N_LINEE_FILE=$(wc -l ${DataDir}/${ProtocolName}_throughput.dat | cut -d' ' -f1)
     NUMERO_LINEA=1
-
-    while [ $NUMERO_LINEA -lt $N_LINEE_FILE ]
+    echo "numero linea:${NUMERO_LINEA} numero linee file: ${N_LINEE_FILE}"
+    
+    #il ciclo arriva  a numero linee file meno 1
+    while [ ${NUMERO_LINEA} -lt ${N_LINEE_FILE} ]
     do 
+        #echo ${NUMERO_LINEA}
         N=$(sed "${NUMERO_LINEA}q;d" ${DataDir}/${ProtocolName}_throughput.dat | cut -d' ' -f1)
-        D=$(bc <<<"scale=20;var1=${L};var2=${N};var3=${B};var1 + var2 / var3")
-        printf "$N $D \n" >> ${OutputDatFile}
-        ((NUMERO_LINEA++))
+        D=$(bc <<<"scale=10;( ${L} + ( ${N} / ${B} ) )")
+        echo "N:${N} D:${D}"
+        Latency_Bandwith=$(bc <<<"scale=10;${N} / ( ${L} + ( ${N} / ${B} ) )")
+        printf "$N ${Latency_Bandwith} \n" >> ${OutputDatFile}
+        NUMERO_LINEA=$(bc <<<"${NUMERO_LINEA}+1")
     done
 
 gnuplot <<-eNDgNUPLOTcOMMAND
@@ -73,7 +66,7 @@ gnuplot <<-eNDgNUPLOTcOMMAND
 	set ylabel "throughput (KB/s)"
 	
 	set output "../data/$OutputPngFile"
-	plot "../data/${OutputDatFile}" using 1:2 title "Latency-Bandwidth model with L=${L0} and B=${B}"\
+	plot "../data/${OutputDatFile}" using 1:2 title "Latency-Bandwidth model with L=${L} and B=${B}"\
 	    with linespoint, \
 	     "../data/${InputFile}" using 1:3 title "${ProtocolName} ping-pong Throughput (average)" \
 			with linespoints
@@ -83,7 +76,6 @@ eNDgNUPLOTcOMMAND
 
 
 done
-
 
 
 
