fb0c8889aa6c0489a07e33cd13d1c3dd53528c18
rollingflamingo <andrea.storace.uni@gmail.com>
2017-12-19 11:18:21 +0100

    fixed main.c pointers

--- incapache-students/main.c
+++ incapache-students/main.c
@@ -109,7 +109,7 @@ void run_webserver(const char *const port_as_str, char *www_root, const int *con
 
 		/*** create PTHREAD number i, running client_connection_thread() ***/
 /*** TO BE DONE 2.2 START ***/
-		client_connection_thread(i);
+		client_connection_thread(&i);
 /*** TO BE DONE 2.2 END ***/
 
 	}
