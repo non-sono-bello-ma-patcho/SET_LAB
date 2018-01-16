4e52af2e8622051862e3961be1e25652be56602e
Strafo <aspem2015@gmail.com>
2018-01-16 21:08:25 +0100

    Add files via upload
    continua a fare per una stringa di richiesta due risposte invece che una

--- incapache-students/autotester.c
+++ incapache-students/autotester.c
@@ -12,24 +12,30 @@ void custom_fail(int *exit_state);
 
 int main(int argc,char *argv [])
 {
-    if(argc!=4)
-    {printf("USAGE:./autotester {webrootdir} {port} {number of test} \n");exit(EXIT_FAILURE);}
+    if(argc!=3)
+    {printf("USAGE:./autotester  {port} {number of test} \n");exit(EXIT_FAILURE);}
     else{printf("*********************INIZIO TEST*********************\n");}
-    int n_test=atoi(argv[3]);
+    int n_test=atoi(argv[2]);
     int *pid=(int*)malloc(sizeof(int)*n_test),exit_state;
     int *status=(int*)malloc(sizeof(int)*n_test);
-    int pipefd[2];
-    char temp[]="GET /images/uncadunca.jpg HTTP/1.1\r\n\r\n\0";
+    int pipefd[2],fd;
+    char message[]="\n****************************Eseguo richiesta telnet****************************\n";
     const char * commands[]={
       "HEAD / HTTP/1.0\r\n\r\n",
       "HEAD /aaa.html HTTP/1.0\r\n\r\n",
-      "HEAAD / HTTP/1.0\r\n",
-      "GET /images/uncadunca.jpg HTTP/1.1\r\n",
-      "GET /images/uncadunca.jpg HTTP/1.1\r\n\r\nIf-Modified-Since: Mon, 26 Nov 2014 11:27:51 GMT\r\n"
+      "HEAAD / HTTP/1.0\r\n\r\n",
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\r\n",
+      "HEAD / HTTP/1.0\r\n\r\n",
+      /*"GET /images/uncadunca.jpg HTTP/1.1\r\n\r\n",
+      "GET /images/uncadunca.jpg HTTP/1.1\r\n\r\nIf-Modified-Since: Mon, 26 Nov 2019 11:27:51 GMT\r\n\r\n",*/
+      NULL
     };
     
     
     /*controllo la grammatica*/
+for(int i=0;commands[i]!=NULL;i++)
+{
+    fflush(stdout);
     if(pipe(pipefd)<0){perror("pipe failed:");exit(EXIT_FAILURE);}
     pid[0]=fork();
     if(pid[0]<0){perror("ERROR:fork failed");}
@@ -37,66 +43,25 @@ int main(int argc,char *argv [])
     {
         dup2(pipefd[0],STDIN_FILENO);
         close(pipefd[1]);close(pipefd[0]);
-        exit_state=execl("/usr/bin/telnet","telnet","localhost",argv[2],(char*)NULL);    
+        exit_state=execl("/usr/bin/telnet","telnet","localhost",argv[1],(char*)NULL);    
         custom_fail(&exit_state);return 0;
     }
     else
     {
         close(pipefd[0]);
-        sleep(3);
-        perror("\nEseguo richiesta telnet:\n");
-        if(write(pipefd[1],temp,strlen(temp))<0){perror("fprintf failed:");exit(EXIT_FAILURE);}
-        perror("aspetto child \n");
+        sleep(2);
+        write(2,message,strlen(message));
+        if(write(pipefd[1],commands[i],strlen(commands[i]))<0){perror("fprintf failed:");exit(EXIT_FAILURE);}
         waitpid(pid[0],&status[0],0);
-    
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-   /*
-    for(int i=0;i<n_test;i++)
-    {
-        if(pipe(pipefd)<0){perror("pipe failed:");exit(EXIT_FAILURE);}
-        pid[i]=fork();
-        if(pid[i]<0){perror("ERROR:fork failed");}
-        if(pid[i]==0)
-        {
-            dup2(pipefd[0], 0);
-            close(pipefd[0]);
-        
-            printf("\nEseguo richiesta[%d]:\n",i+1);
-            exit_state_client=execl("/usr/bin/telnet","telnet","localhost",argv[2],(char*)NULL);    
-            custom_fail(&exit_state);return 0;
-        }
-        else
-        {
-            dup2(pipefd[1], 1); 
-            close(pipefd[1]);
-        }
     }
     
-    for(int i=0;i<l_str_test;i++)
-    {
-        if(write(stdout,command[i])<0){{perror("write failed:");exit(EXIT_FAILURE);}
-    
-    
-    
-    }
+}
+system("clear");
 
-    for(int i=0;i<n_test;i++)
-    {
-        waitpid(pid[i],&status[i],0);}
-    }*/
+
+
+
+
 
 printf("TEST TERMINATO!\n");
 return 0;
